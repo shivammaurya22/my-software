4e841a0578109ffb1363c261318bbf40c06c54f1
shivammaurya22 <shivmmaurya2292005@gmail.com>
2025-04-03 18:11:42 +0530

    My Task App

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='text-4xl text-purple-700 font-bold'>
+      My Dash Board
+    </div>
+  )
+}
+
+export default Dashboard
+
